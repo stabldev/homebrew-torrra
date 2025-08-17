d659b235aa8e01bd0c8f3b5857e6c7ae85829cd5
stabldev <114811070+stabldev@users.noreply.github.com>
2025-08-17 12:02:25 +0530

    Revert "chore: update versions"
    This reverts commit b902708db18009c898b35a19b1233f4f454e84f5.

--- torrra.rb
+++ torrra.rb
@@ -2,13 +2,13 @@ class Torrra < Formula
   version "1.2.7"
   sha256 "8845565acad5e8a617dd589a8bbfac6eac6e04fb9c39d18737fdfeb787b5dc87"
 
-  url "https://github.com/stabldev/torrra/releases/download/v1.2.7/torrra_v1.2.7_macos_arm64"
+  url "https://github.com/stabldev/torrra/releases/download/v1.2.4/torrra_v1.2.4_macos_arm64"
   desc "A Python tool that lets you find and download torrents without leaving your CLI."
   name "Torrra"
   homepage "https://github.com/stabldev/torrra"
 
   def install
-    bin.install "torrra_v1.2.7_macos_arm64" => "torrra"
+    bin.install "torrra_v1.2.4_macos_arm64" => "torrra"
   end
 
   test do
